7626f364c44cbdfdb69dc05f5d1c17eec719376c
Evsey Kirichkov <evseykirichkov@mail.ru>
2022-10-07 15:43:23 +0300

    correct4

--- homework.py
+++ homework.py
@@ -73,6 +73,7 @@ class Running(Training):
             * self.weight / self.M_IN_KM * (self.duration * 60)
         )
 
+
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
@@ -111,7 +112,8 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        return (self.length_pool * self.count_pool
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
